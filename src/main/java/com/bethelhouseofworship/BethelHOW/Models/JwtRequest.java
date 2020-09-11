53c3bf95ad3230b8a2bad83b7ea0dfd5670ee15b
David Trombello <dtrombello@gmail.com>
2020-09-11 01:31:18 -0400

    adding jwt classes

---
+++ src/main/java/com/bethelhouseofworship/BethelHOW/Models/JwtRequest.java
@@ -0,0 +1,4 @@
+package com.bethelhouseofworship.BethelHOW.Models;
+
+public class JwtRequest {
+}
---
+++ src/main/java/com/bethelhouseofworship/BethelHOW/Models/JwtResponse.java
@@ -0,0 +1,4 @@
+package com.bethelhouseofworship.BethelHOW.Models;
+
+public class JwtResponse {
+}
